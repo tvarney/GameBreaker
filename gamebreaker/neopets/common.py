1d1e2cc9ea4a2417e3436c1bd6b9a44edc887685
tvarney <troy.a.varney@gmail.com>
2017-05-09 15:43:17 -0500

    Changed game area finding code to take a threshold value (default to 0.85)

--- gamebreaker/neopets/common.py
+++ gamebreaker/neopets/common.py
@@ -2,7 +2,7 @@
 import gamebreaker.image as image
 
 
-def GetGameArea():
+def GetGameArea(match_value: float = 0.85):
     im = image.ImageGrabber()
 
     img_ul = image.load_from_file("./data/neopets_top_left.png", True)
@@ -11,13 +11,14 @@ def GetGameArea():
     match_ul = image.find_match(screen, img_ul, image.TM_CCOEFF_NORMED)
     match_br = image.find_match(screen, img_br, image.TM_CCOEFF_NORMED)
 
-    if match_ul[0] < 0.95 or match_br[0] < 0.95:
+    if match_ul[0] < match_value or match_br[0] < match_value:
+        print("UL_Value: {}\nBR_Value: {}".format(match_ul[0], match_br[0]))
         return None
 
     top_left = match_ul[1]
     bottom_right = match_br[1]
     ul_w, ul_h, br_w, br_h = img_ul.shape[0], img_ul.shape[1], img_br.shape[0], img_br.shape[1]
-    top_left = top_left[0] + ul_h, top_left[1] +  ul_w
+    top_left = top_left[0] + ul_h, top_left[1] + ul_w
 
     bbox = im.make_bbox(top_left[0], top_left[1], bottom_right[0] - top_left[0], bottom_right[1] - top_left[1])
     return bbox
